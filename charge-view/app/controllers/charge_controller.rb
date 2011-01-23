2d1e32522d601d7f169f2eb4dc35a1318991e6e9
willibi <gilles.ballanger@free.fr>
2011-01-23 01:03:22 +0100

    bug fix in workload computation improve x and y axis label representation

--- charge-view/app/controllers/charge_controller.rb
+++ charge-view/app/controllers/charge_controller.rb
@@ -9,7 +9,7 @@ class ChargeController < ApplicationController
   def index
     @users = User.find(:all,
                        :order => "firstName",
-                       :limit => 10)
+                       :limit => 40)
   end
   
   def data
@@ -40,8 +40,10 @@ class ChargeController < ApplicationController
       sep = ","
     end
     json +=      '] } ],'
-    json +=  '"y_axis":{"min": 0,"max": 160, "steps": 20},'
-    json +=  '"x_axis":{"labels":{"labels":['
+    step = ((workload.max_hour.to_f/5.0).divmod(10)[0]+1)*10
+    max = (workload.max_hour.to_f.divmod(step)[0]+1)*step
+    json +=  '"y_axis":{"min": 0,"max": ' + max.to_s + ', "steps": ' + step.to_s + '},'
+    json +=  '"x_axis":{"labels":{"rotate": -45,"labels":['
     sep = ""
     datas.keys.sort.each do |date|
       json += sep + '"' + date.to_s + '"'
@@ -102,6 +104,9 @@ class ChargeController < ApplicationController
       @result = {}
       
       @issue_list = []
+      
+      @max_hour_value = 0.0
+      
     end
     
     # add an issue to workload
@@ -113,7 +118,11 @@ class ChargeController < ApplicationController
     def compute_data
       # init data result with period, obs_start_date and obs_end_date
       @result = {}
-      
+       (@obs_start_date..@obs_end_date).each do |date|
+        # get period start date according to period_type
+        p_date = get_period_date(date)
+        @result[p_date] = 0.0
+      end
       puts 'compute ' + @issue_list.length.to_s + ' issues'
       
       # parse each issue
@@ -129,26 +138,40 @@ class ChargeController < ApplicationController
         # compute issue stop date
         due_date = issue.due_before
         
+        if due_date.nil?
+          due_date = start_date + 1
+        end
+        
         # compute working days between the 2 date
         workabledays = (start_date..due_date).select{ |d| WEEKDAY_NUMBERS.include?( d.wday ) }.length.to_f
+        if workabledays == 0
+          workabledays = 1
+        end
         
         # for each working days
          (start_date..due_date).each do |date|
           # get period start date according to period_type
           p_date = get_period_date(date)
-          
-          # add hours working date hours to result
-          hours_for_day = issue.estimated_hours.to_f / workabledays
-          if @result[p_date].nil?
-            @result[p_date] = hours_for_day
-          else
-            @result[p_date] += hours_for_day
-          end
-            
+          if p_date >= get_period_date(@obs_start_date) and p_date <= get_end_period_date(@obs_end_date)
+            # add hours working date hours to result
+            hours_for_day = issue.estimated_hours.to_f / workabledays
+            if @result[p_date].nil?
+              @result[p_date] = hours_for_day
+            else
+              @result[p_date] += hours_for_day
+            end
+            if @result[p_date] > @max_hour_value
+              @max_hour_value = @result[p_date] 
+            end
+          end          
         end
       end
       puts "result : " + @result.inspect
       return @result
+    end
+    
+    def max_hour
+      return @max_hour_value
     end
     
     def get_period_date(date)
